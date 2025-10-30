69a16cd9ad22e62c7eb0091b161b8629d2989743
Shaan Rao/Ryan <shaanrao1002@gmail.com>
2025-10-30 09:01:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const users = {}; // username -> { socketId, publicKeyPem }
+const users = {}; 
 
 io.on('connection', (socket) => {
   console.log('Client connected:', socket.id);
